d8f54fe56e6bef5d6ec44469a4caaaaa91b22e97
evanbondura <ebondur@clemson.edu>
2022-12-03 17:16:45 -0500

    Getting Started

---
+++ Source/Bowling_Simulation/Bowling_Simulation.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Bowling_Simulation.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Bowling_Simulation, "Bowling_Simulation" );
---
+++ Source/Bowling_Simulation/Bowling_SimulationGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Bowling_SimulationGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class BOWLING_SIMULATION_API ABowling_SimulationGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
